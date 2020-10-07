80a962803e7e7d06447dccecf973a73bf198bb57
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-10-07 13:50:23 +0200

    Update configs

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,9 @@ const { task, src, dest } = require('gulp');
 const ts = require('gulp-typescript');
 
 task('ts', () => src('src/**/*.ts')
-  .pipe(ts())
+  .pipe(ts({
+    target: 'es5',
+    module: 'commonjs',
+    declaration: true,
+  }))
   .pipe(dest('lib')));
