68c69f8fa9879b31e61248951aa11086a3634446
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2020-10-11 15:43:10 +0200

    Remove unused gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,4 @@
-const {
-  task, src, dest, series,
-} = require('gulp');
-const ts = require('gulp-typescript');
+const { task } = require('gulp');
 const del = require('del');
 
 task('clean', () => del('lib/**', { force: true }));
-
-task('build', () => src(['src/**/*.ts', '!src/**/*.test.ts', '!src/**/__mock__'])
-  .pipe(ts({
-    target: 'es5',
-    module: 'commonjs',
-    declaration: true,
-    experimentalDecorators: true,
-  }))
-  .pipe(dest('lib')));
-
-exports.default = series('clean', 'build');
